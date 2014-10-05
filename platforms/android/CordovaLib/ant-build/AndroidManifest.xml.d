c70ea0a9611e32673f68b388f695ea9a954ffd5c
rezeile008 <elias.marco@gmail.com>
2014-10-05 13:40:55 +0800

    Second Add

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+E:\Dev Projects\Phonegap\eliasteroid\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+E:\Dev Projects\Phonegap\eliasteroid\platforms\android\CordovaLib\AndroidManifest.xml \
